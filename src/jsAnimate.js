97d9a8f0e19baf80748acb461fac782bf9ceac70
LiuTianYu <1211125661@qq.com>
2019-06-25 09:49:27 +0800

    通过适配器模式升级slick和slide的api

--- src/jsAnimate.js
+++ src/jsAnimate.js
@@ -10,6 +10,25 @@ export default window.jsAnimate = function () {
     //绑定类似animate.css的懒加载过渡效果
     eventListener(window, "load", fade)
 
+    //slick和slide函数的适配器，适用于同个页面有多个旋转木马效果或轮播图的场景，且它们使用同样的配置参数
+    function adapter(method) {
+        return function adapterMehtod(idArray, config) {
+            if (arguments.length === 1 && Object.prototype.toString.call(arguments[0]) === "[object Object]") {
+                method(arguments[0])
+                return
+            }
+            if (!Array.isArray(idArray)) {
+                throw new Error(`${idArray} is not array`)
+            }
+            if (Object.prototype.toString.call(config) !== "[object Object]") {
+                throw new Error(`${config} is not object`)
+            }
+            idArray.forEach(function (id) {
+                method(Object.assign({ id: id }, config))
+            })
+        }
+    }
+
     //返回一个对象object <method>
-    return { show: show, hide: hide, returnTop: returnTop, slide: slide, slick: slick }
+    return { show: show, hide: hide, returnTop: returnTop, slide: adapter(slide), slick: adapter(slick) }
 }()
